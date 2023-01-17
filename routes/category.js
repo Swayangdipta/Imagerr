6998f9ff2c523653e4aa53887689b52065f9f33a
Swayangdipta Das <swayangdiptacc@gmail.com>
2023-01-17 10:24:29 +0530

    Added authentication on category routes

--- routes/category.js
+++ routes/category.js
@@ -1,12 +1,15 @@
 const router = require("express").Router()
+const { isSignedIn, isAuthenticated, isAdmin } = require("../controllers/auth")
 const { getCategoryById, getACategory, getAllCategories, createCategory, updateCategory, deleteCategory } = require("../controllers/category")
+const { getUserById } = require("../controllers/user")
 
 router.param("categoryId",getCategoryById)
+router.param("userId",getUserById)
 
 router.get('/category/:categoryId',getACategory)
 router.get('/category',getAllCategories)
-router.post('/category',createCategory)
-router.put('/category/:categoryId',updateCategory)
-router.delete('/category/:categoryId',deleteCategory)
+router.post('/category/:userId',isSignedIn,isAuthenticated,isAdmin,createCategory)
+router.put('/category/:categoryId/:userId',isSignedIn,isAuthenticated,isAdmin,updateCategory)
+router.delete('/category/:categoryId/:userId',isSignedIn,isAuthenticated,isAdmin,deleteCategory)
 
 module.exports = router
